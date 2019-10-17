2b829f1edfa0be6a505730282c1e29d558a384ef
tuomashjelt <36914564+tuomashjelt@users.noreply.github.com>
2019-10-17 11:42:10 +0300

    Formatting

--- server.R
+++ server.R
@@ -1,15 +1,8 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+# This is the server logic of a Shiny web application.
 
 library(shiny)
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw plots
 shinyServer(function(input, output) {
    
         output$chosenPlot <- renderPlot({
